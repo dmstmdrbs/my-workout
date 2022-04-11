3ff4ec861a1c8e26567b0a789fb09b598e4c77cb
dmstmdrbs <dmstmdrbs98@gmail.com>
2022-04-11 16:51:12 +0900

    Add ThemeProvider

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import App from './src/App';
 import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from '@emotion/react';
 import { NavigationContainer } from '@react-navigation/native';
+import { theme } from './src/styles/theme';
 
 export default () => {
   return (
     <RecoilRoot>
-      <NavigationContainer>
-        <App />
-      </NavigationContainer>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>
+          <App />
+        </NavigationContainer>
+      </ThemeProvider>
     </RecoilRoot>
   );
 };
